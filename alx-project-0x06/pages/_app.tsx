8b5d7f3f606bf84b30c3de28479fd321ce12db9a
SiphoVokal <163132466+SiphoVokal@users.noreply.github.com>
2025-07-28 15:57:28 +0200

    store

--- alx-project-0x06/pages/_app.tsx
+++ alx-project-0x06/pages/_app.tsx
@@ -2,14 +2,18 @@
 import Layout from "@/components/layouts/Layout";
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import { Provider } from "react-redux";
 import { CountProvider } from "@/context/CountContext";
+import store from "@/store/store";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <CountProvider>
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
-    </CountProvider>
+    <Provider store={store}>
+      <CountProvider>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </CountProvider>
+    </Provider>
   );
 }
